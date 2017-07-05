3e913770ad564704efb0cbc37b096726cbdd92af
gw <1012006716@qq.com>
2017-07-05 21:33:27 +0800

    cinema 7.5 pm 01

---
+++ src/components/CinemaHeader/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="cinema_header">
+        <div>
+            <img src="../../assets/iv_back_blue" alt="" v-if="hasBackIcon">
+            <span>{{title}}</span>
+        </div>
+        <i class="iconfont icon-chazhao"></i>
+
+        <div class="c_h_border"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props : {
+            hasBackIcon : {
+                type : Boolean,
+                default : true
+            },
+            title : {
+                type : String,
+                default : "影院"
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/CinemaList/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="cinema_list_container">
+        
+    </div>
+</template>
+
+<script>
+    import Scroll from "../../components/Scroll";
+    export default {
+        props : {
+
+        },
+        data () {
+            return {
+
+            }
+        },
+        methods : {
+
+        },
+        watch : {
+
+        },
+        components : {
+            "scroll" : Scroll
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    
+</style>
